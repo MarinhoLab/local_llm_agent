524e7708848acd0c5c229c6b3272b8bd9759628f
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-08-10 13:37:44 +0100

    Adjusting tool parser, now working

--- dgx_spark_host/entrypoint.sh
+++ dgx_spark_host/entrypoint.sh
@@ -10,4 +10,4 @@ set -euo pipefail
 : "${MAX_NUM_SEQS:=1}"
 : "${SERVED_MODEL_NAME:=qwen-local}"
 
-exec vllm serve "${MODEL_NAME}"   --host "${HOST}"   --port "${PORT}"   --dtype auto   --served-model-name "${SERVED_MODEL_NAME}"   --max-model-len "${MAX_MODEL_LEN}"   --gpu-memory-utilization "${GPU_MEMORY_UTILIZATION}"   --max-num-seqs "${MAX_NUM_SEQS}"   --enable-prefix-caching   --enable-auto-tool-choice --tool-call-parser hermes --api-key "${API_KEY}"
+exec vllm serve "${MODEL_NAME}"   --host "${HOST}"   --port "${PORT}"   --dtype auto   --served-model-name "${SERVED_MODEL_NAME}"   --max-model-len "${MAX_MODEL_LEN}"   --gpu-memory-utilization "${GPU_MEMORY_UTILIZATION}"   --max-num-seqs "${MAX_NUM_SEQS}"   --enable-prefix-caching   --enable-auto-tool-choice --tool-call-parser qwen3_coder --api-key "${API_KEY}"

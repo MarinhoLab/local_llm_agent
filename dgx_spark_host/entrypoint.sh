124d0d6afa33d82e386eb93098ff12939d1008b5
Murilo M. Marinho <murilo.marinho@manchester.ac.uk>
2026-09-11 10:22:07 +0100

    Merged some of the settings with the ones in the model card.

--- dgx_spark_host/entrypoint.sh
+++ dgx_spark_host/entrypoint.sh
@@ -6,7 +6,6 @@ SPECULATIVE_CONFIG='{"method":"'"${SPEC_METHOD}"'","num_speculative_tokens":'"${
 exec vllm serve "${MODEL_NAME}"   \
 --host "${HOST}" \
 --port "${PORT}" \
---dtype auto \
 --limit-mm-per-prompt '{"image":4}' \
 --served-model-name "${SERVED_MODEL_NAME}" \
 --max-model-len "${MAX_MODEL_LEN}" \
@@ -14,12 +13,10 @@ exec vllm serve "${MODEL_NAME}"   \
 --max-num-seqs "${MAX_NUM_SEQS}" \
 --max-num-batched-tokens "${MAX_NUM_BATCHED_TOKENS}" \
 --trust-remote-code \
---kv-cache-dtype fp8 \
+--seed 0 \
+--kv-cache-dtype fp8_e4m3 \
 --enable-chunked-prefill \
---async-scheduling \
 --enable-prefix-caching \
---speculative-config "${SPECULATIVE_CONFIG}" \
---load-format fastsafetensors \
 --enable-auto-tool-choice \
 --tool-call-parser qwen3_coder \
 --reasoning-parser qwen3 \
